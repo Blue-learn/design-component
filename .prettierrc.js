0c50ba889e3b9484fc164acd6082b98ad782922e
soumyasethy <sethy.soumyaranjan@gmail.com>
2022-04-24 01:29:54 +0530

    add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	printWidth: 50,
+	trailingComma: 'all',
+	tabWidth: 1,
+	semi: true,
+	useTabs: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	bracketSpacing: true,
+	arrowParens: 'always',
+	parser: 'typescript',
+	jsxBracketSameLine: false,
+};
