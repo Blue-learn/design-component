c9af286d4e6b61b14eb883dba3e5507999493fad
soumyasethy <sethy.soumyaranjan@gmail.com>
2022-04-23 19:21:40 +0530

    refactor

--- packages/component/src/button/types.d.ts
+++
@@ -1,6 +0,0 @@
-import { ButtonProps } from "@design-component/schema";
-import React from "react";
-
-export declare type ButtonComponentType = ((props: ButtonProps) => JSX.Element) & {
-    Shimmer?: React.FunctionComponent<() => JSX.Element>;
-};
