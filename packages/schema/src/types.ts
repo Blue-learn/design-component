9a2ac57604ed7a37159d9f9cfba0057e33f3a3d1
soumyasethy <sethy.soumyaranjan@gmail.com>
2022-04-26 20:27:24 +0530

    refactor

--- packages/schema/src/types.ts
+++ packages/schema/src/types.ts
@@ -5,5 +5,14 @@ type NormalWidgetItem = {
 	type: WIDGET;
 	props: any;
 };
-
 export type WidgetItem = NormalWidgetItem;
+export type WidgetProps = {
+	renderItem?(item: WidgetItem): any;
+	isVisible?: boolean;
+	modelUpdateWrapper?(prop: Function): Function;
+	performAction?(action: TapAction): void;
+};
+export type TapAction = {
+	type: string;
+	data: any;
+};
--- packages/theme/src/light-mobile/button/index.ts
+++ packages/theme/src/light-mobile/button/index.ts
@@ -10,12 +10,12 @@ export const buttonTypeTokensMap: ButtonTypeTokensMap = {
     size: ButtonSize.Big,
     borderRadius: 16,
     textColor: ColorTokens.White,
-    bgColor: ColorTokens.Blue_60,
+    bgColor: ColorTokens.Blue_50,
   },
   [ButtonTypeTokens.SmallFilled]: {
     size: ButtonSize.Big,
     borderRadius: 16,
     textColor: ColorTokens.White,
-    bgColor: ColorTokens.Blue_60,
+    bgColor: ColorTokens.Blue_50,
   },
 };
