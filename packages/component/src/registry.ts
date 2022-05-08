27f4d3041d6fc176ac624cb92ffa407318a870cb
Darshil Rathod <69842641+RathodDarshil@users.noreply.github.com>
2022-05-09 01:38:24 +0530

    Icon add to registry

--- packages/component/src/registry.ts
+++ packages/component/src/registry.ts
@@ -1,7 +1,9 @@
 import { Button } from '../src';
-import {WIDGET} from '@blue-learn/schema';
+import { WIDGET } from '@blue-learn/schema';
+import { Icon } from '../src';
 
 const registry = {
 	[WIDGET.BUTTON]: Button,
+	[WIDGET.ICON]: Icon,
 };
 export { registry as WidgetRegistry };
