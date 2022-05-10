164a74e08910c7766a8201facafa7dbcc1e20d9d
saidatta-bluelearn <saidatta@bluelearn.in>
2022-05-10 13:24:00 +0530

    search icon issue fixed

--- packages/component/src/icon/icon-list/Search.tsx
+++ packages/component/src/icon/icon-list/Search.tsx
@@ -1,10 +1,5 @@
 import * as React from 'react';
-import Svg, {
-	ClipPath,
-	Defs,
-	G,
-	Path,
-} from 'react-native-svg';
+import Svg, { Path } from 'react-native-svg';
 
 function SeacrchIcon(props) {
 	return (
@@ -16,17 +11,10 @@ function SeacrchIcon(props) {
 			xmlns='http://www.w3.org/2000/svg'
 			{...props}
 		>
-			<G clipPath='url(#clip0_373_4080)'>
-				<Path
-					d='M17.78 16.72l-4.477-4.477a7.512 7.512 0 10-1.06 1.06l4.476 4.477a.75.75 0 001.06-1.06zM7.5 13.5a6 6 0 116-6 6.007 6.007 0 01-6 6z'
-					fill={props.color}
-				/>
-			</G>
-			<Defs>
-				<ClipPath id='clip0_373_4080'>
-					<Path fill='#fff' d='M0 0H18V18H0z' />
-				</ClipPath>
-			</Defs>
+			<Path
+				d='M17.78 16.72l-4.477-4.477a7.512 7.512 0 10-1.06 1.06l4.476 4.477a.75.75 0 001.06-1.06zM7.5 13.5a6 6 0 116-6 6.007 6.007 0 01-6 6z'
+				fill={props.color}
+			/>
 		</Svg>
 	);
 }
