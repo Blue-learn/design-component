40bd2f8b11acc75991e5e06dfb7bc85a092d20b9
soumyasethy <sethy.soumyaranjan@gmail.com>
2022-05-25 13:49:59 +0530

    add ListView, render DC

--- packages/component/src/index.ts
+++ packages/component/src/index.ts
@@ -3,9 +3,9 @@ export * as Typography from './typography';
 export * as TextInput from './textInput';
 export * as Space from './space';
 export * as Icon from './icon';
-export * from './stack';
+export * as Stack from './stack';
 export * as Avatar from './avatar';
 export * from './tabs';
 export * from './mock';
 export * from './registry';
-export * from "./divider"
+export * from './divider';
