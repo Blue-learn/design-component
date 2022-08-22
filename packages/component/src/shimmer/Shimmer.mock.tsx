90b984894aabea30c19e7d67cee063c9a0d84a9f
Saidatta Sahu <saidatta@bluelearn.in>
2022-08-22 15:32:32 +0530

    shimmer bordder

--- packages/component/src/shimmer/Shimmer.mock.tsx
+++ packages/component/src/shimmer/Shimmer.mock.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { ShimmerProps } from '@blue-learn/schema';
+import {
+	BorderRadiusTokens,
+	ShimmerProps,
+} from '@blue-learn/schema';
 
 export const args: ShimmerProps = {
 	width: 400,
 	height: 400,
-	borderRadius: 10,
+	borderRadius: BorderRadiusTokens.BR1,
 };
