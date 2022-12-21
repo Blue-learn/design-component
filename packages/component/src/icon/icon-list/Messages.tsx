504ba1c689170ca6bddebbb2d505cdd1b8e16055
Saidatta Sahu <saidatta@bluelearn.in>
2022-12-21 12:39:23 +0530

    size and stoke fix

--- packages/component/src/icon/icon-list/Messages.tsx
+++ packages/component/src/icon/icon-list/Messages.tsx
@@ -14,7 +14,7 @@ function Messages(props) {
 			<Path
 				d='M17.98 10.79v4c0 .26-.01.51-.04.75-.23 2.7-1.82 4.04-4.75 4.04h-.4c-.25 0-.49.12-.64.32l-1.2 1.6c-.53.71-1.39.71-1.92 0l-1.2-1.6a.924.924 0 00-.64-.32h-.4C3.6 19.58 2 18.79 2 14.79v-4c0-2.93 1.35-4.52 4.04-4.75.24-.03.49-.04.75-.04h6.4c3.19 0 4.79 1.6 4.79 4.79z'
 				stroke={props.color}
-				strokeWidth={2}
+				strokeWidth={1.5}
 				strokeMiterlimit={10}
 				strokeLinecap='round'
 				strokeLinejoin='round'
@@ -22,7 +22,7 @@ function Messages(props) {
 			<Path
 				d='M21.98 6.79v4c0 2.94-1.35 4.52-4.04 4.75.03-.24.04-.49.04-.75v-4c0-3.19-1.6-4.79-4.79-4.79h-6.4c-.26 0-.51.01-.75.04C6.27 3.35 7.86 2 10.79 2h6.4c3.19 0 4.79 1.6 4.79 4.79z'
 				stroke={props.color}
-				strokeWidth={2}
+				strokeWidth={1.5}
 				strokeMiterlimit={10}
 				strokeLinecap='round'
 				strokeLinejoin='round'
--- packages/component/src/icon/icon-list/Search.tsx
+++ packages/component/src/icon/icon-list/Search.tsx
@@ -9,8 +9,8 @@ import Svg, {
 function SearchIcon(props) {
 	return (
 		<Svg
-			width={24}
-			height={24}
+			width={props.size}
+			height={props.size}
 			viewBox='0 0 24 24'
 			fill='none'
 			xmlns='http://www.w3.org/2000/svg'
