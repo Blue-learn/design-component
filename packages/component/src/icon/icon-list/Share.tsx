df88ec0fdeb471e219c5b9fd967399e590f03628
Saidatta Sahu <saidatta@bluelearn.in>
2023-04-11 17:39:29 +0530

    Update : share icon changed

--- packages/component/src/icon/icon-list/Share.tsx
+++ packages/component/src/icon/icon-list/Share.tsx
@@ -9,25 +9,25 @@ function Share(props) {
 		<Svg
 			width={props.size}
 			height={props.size}
-			viewBox='0 0 25 24'
+			viewBox='0 0 24 25'
 			fill='none'
 			xmlns='http://www.w3.org/2000/svg'
 			{...props}
 		>
 			<Path
-				d='M21.25 5.779a2.36 2.36 0 11-4.722 0 2.36 2.36 0 014.721 0zM8.807 12a2.36 2.36 0 11-4.721 0 2.36 2.36 0 014.721 0z'
+				d='M20.582 6.279a2.36 2.36 0 11-4.721 0 2.36 2.36 0 014.72 0zM8.14 12.5a2.36 2.36 0 11-4.722 0 2.36 2.36 0 014.721 0z'
 				stroke={props.color}
 				strokeWidth={1.5}
 			/>
 			<Circle
-				cx={18.8889}
-				cy={18.2211}
+				cx={18.2211}
+				cy={18.7211}
 				r={2.36056}
 				stroke={props.color}
 				strokeWidth={1.5}
 			/>
 			<Path
-				d='M16.297 6.816l-7.258 4.147m0 2.074l7.258 4.147'
+				d='M15.63 7.316L8.37 11.463m0 2.074l7.258 4.147'
 				stroke={props.color}
 				strokeWidth={1.5}
 				strokeLinecap='round'
